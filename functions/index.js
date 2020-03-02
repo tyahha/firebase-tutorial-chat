37642df456b96debd363aea8754553b0e0ae34af
toshinori sato <tyahhahha@gmail.com>
2020-03-03 00:53:53 +0900

    add new message notification

--- functions/index.js
+++ functions/index.js
@@ -42,4 +42,65 @@ exports.addWelcomeMessages = functions.auth.user().onCreate(async user => {
 
 // TODO(DEVELOPER): Write the blurOffensiveImages Function here.
 
-// TODO(DEVELOPER): Write the sendNotifications Function here.
+// Write the sendNotifications Function here.
+exports.sendNotifications = functions.firestore
+  .document("messages/{messageId}")
+  .onCreate(async snapshot => {
+    // Notification details.
+    const text = snapshot.data().text;
+    const payload = {
+      notification: {
+        title: `${snapshot.data().name} posted ${
+          text ? "a message" : "an image"
+        }`,
+        body: text
+          ? text.length <= 100
+            ? text
+            : text.substring(0, 97) + "..."
+          : "",
+        icon:
+          snapshot.data().profilePicUrl || "/images/profile_placeholder.png",
+        click_action: `http://${process.env.GCLOUD_PROJECT}.firebaseapp.com`
+      }
+    };
+
+    // Get the list of device tokens.
+    const allTokens = await admin
+      .firestore()
+      .collection("fcmTokens")
+      .get();
+    const tokens = [];
+    allTokens.forEach(tokenDoc => {
+      tokens.push(tokenDoc.id);
+    });
+
+    if (tokens.length > 0) {
+      // Send notifications to all tokens.
+      const response = await admin.messaging().sendToDevice(tokens, payload);
+      await cleanupTokens(response, tokens);
+      console.log("Notifications have been sent and tokens cleaned up.");
+    }
+  });
+
+function cleanupTokens(response, tokens) {
+  const tokensDelete = [];
+  response.results.forEach((result, index) => {
+    const error = result.error;
+    if (error) {
+      console.log("Failure sending notification to", tokens[index], error);
+      // Cleanup the tokens who are not registered anumore.
+      if (
+        error.code === "messaging/invalid-registration-token" ||
+        error.code === "messaging/registration-token-not-registered"
+      ) {
+        const deleteTask = admin
+          .firestore()
+          .collection("messages")
+          .doc(tokens[index])
+          .delete();
+        tokensDelete.push(deleteTask);
+      }
+    }
+  });
+  return Promise.all(tokensDelete);
+}
